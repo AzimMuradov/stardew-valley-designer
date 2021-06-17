710af001fd357d4151d67e5a2896162ea79506c1
Azim Muradov <azim.muradov.dev@gmail.com>
2021-06-17 22:35:26 +0300

    Add buttons for Menu.kt

--- src/main/kotlin/gui/screens/Menu.kt
+++ src/main/kotlin/gui/screens/Menu.kt
@@ -17,26 +17,83 @@
 package gui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 object Menu : Screen {
 
+    private val padding = 20.dp
+    private val buttonColor = Color(0xFFF4D21F)
+
     @Composable
     operator fun invoke(sh: ScreenHandler) {
         Row(Modifier.fillMaxSize()) {
-            Column(Modifier.weight(2f).fillMaxHeight().background(color = Color.Blue)) {
+            Column(Modifier.weight(1f).fillMaxHeight().background(color = Color(0xFF6B4A37))) {
+                NewPlanButton()
+                PlansButton()
+                Spacer(Modifier.weight(2f).fillMaxWidth().padding(padding))
+                Row(Modifier.weight(1f).fillMaxWidth()) {
+                    ResourcesButton()
+                    SettingsButton()
+                }
+                Row(Modifier.weight(1f).fillMaxWidth()) {
+                    AboutButton()
+                    DonateButton()
+                }
+            }
+            Column(Modifier.weight(2f).fillMaxHeight().background(color = Color(0xFFBF9999))) {
                 //
             }
-            Column(Modifier.weight(5f).fillMaxHeight().background(color = Color.Cyan)) {
-                //
-            }
+        }
+    }
+
+
+    @Composable
+    fun ColumnScope.NewPlanButton() {
+        Box(Modifier.weight(1f).fillMaxSize().padding(padding)) {
+            IconButton(modifier = Modifier.fillMaxSize().background(buttonColor), onClick = {}) { Text("New Plan") }
+        }
+    }
+
+    @Composable
+    fun ColumnScope.PlansButton() {
+        Box(Modifier.weight(1f).fillMaxSize().padding(padding)) {
+            IconButton(modifier = Modifier.fillMaxSize().background(buttonColor), onClick = {}) { Text("Plans") }
+        }
+    }
+
+
+    @Composable
+    fun RowScope.ResourcesButton() {
+        Box(Modifier.weight(1f).fillMaxSize().padding(padding)) {
+            IconButton(modifier = Modifier.fillMaxSize().background(buttonColor), onClick = {}) { Text("Resources") }
+        }
+    }
+
+    @Composable
+    fun RowScope.SettingsButton() {
+        Box(Modifier.weight(1f).fillMaxSize().padding(padding)) {
+            IconButton(modifier = Modifier.fillMaxSize().background(buttonColor), onClick = {}) { Text("Settings") }
+        }
+    }
+
+    @Composable
+    fun RowScope.AboutButton() {
+        Box(Modifier.weight(1f).fillMaxSize().padding(padding)) {
+            IconButton(modifier = Modifier.fillMaxSize().background(buttonColor), onClick = {}) { Text("About") }
+        }
+    }
+
+    @Composable
+    fun RowScope.DonateButton() {
+        Box(Modifier.weight(1f).fillMaxSize().padding(padding)) {
+            IconButton(modifier = Modifier.fillMaxSize().background(buttonColor), onClick = {}) { Text("Donate") }
         }
     }
 }
