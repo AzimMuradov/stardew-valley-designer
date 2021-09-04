e66a5057270a32e650a5517c1f1f5fd1976d741c
Azim Muradov <azim.muradov.dev@gmail.com>
2021-09-04 15:56:29 +0300

    Update Kotlin and Compose Desktop

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -2,8 +2,8 @@ object V {
 
     // Plugins
 
-    const val P_KOTLIN: String = "1.5.21"
-    const val P_COMPOSE: String = "1.0.0-alpha3"
+    const val P_KOTLIN: String = "1.5.30"
+    const val P_COMPOSE: String = "1.0.0-alpha4-build331"
 
 
     // Kotlin Compile
