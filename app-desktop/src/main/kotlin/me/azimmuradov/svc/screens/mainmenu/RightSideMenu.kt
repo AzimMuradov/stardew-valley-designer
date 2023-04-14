359f0867fb7bade0a0f57d723f22c518d9c12ae1
Azim Muradov <azim.muradov.dev@gmail.com>
2023-04-14 10:40:08 +0300

    Separate `MainMenuUi` into several composables, add some icons, prettify UI

---
+++ app-desktop/src/main/kotlin/me/azimmuradov/svc/screens/mainmenu/RightSideMenu.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2021-2023 Azim Muradov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.azimmuradov.svc.screens.mainmenu
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun RowScope.RightSideMenu(onCartographerScreenCall: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .weight(1f)
+            .shadow(
+                elevation = 4.dp,
+                shape = MaterialTheme.shapes.large
+            )
+            .background(MaterialTheme.colors.secondaryVariant)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Row(Modifier.fillMaxWidth().height(56.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+            Button(onClick = onCartographerScreenCall, Modifier.fillMaxHeight().width(200.dp)) {
+                Text(text = "New Plan")
+            }
+
+            Button(onClick = {}, Modifier.fillMaxHeight().width(200.dp)) {
+                Text(text = "Import plan from save")
+            }
+
+            Button(onClick = {}, Modifier.fillMaxHeight().width(200.dp), enabled = false) {
+                Text(text = "Search for a plan")
+            }
+        }
+
+
+        // TODO : Persistent Plans
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.surface),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            // items(count = 5) {
+            //     Card(modifier = Modifier.fillMaxWidth().height(200.dp)) {
+            //         Row(
+            //             modifier = Modifier.fillMaxSize().padding(8.dp),
+            //             horizontalArrangement = Arrangement.spacedBy(8.dp)
+            //         ) {
+            //             Card(Modifier.fillMaxHeight().width(120.dp)) {
+            //                 Text(text = "Plan screenshot")
+            //             }
+            //             Card(Modifier.fillMaxHeight().weight(1f)) {
+            //                 Column {
+            //                     Text(text = "Plan #$it")
+            //                     Text(text = "Plan info")
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
+        }
+    }
+}
