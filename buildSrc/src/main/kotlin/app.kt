d450b6b5f60e79a3d876d92b51ba73ad58ab213e
Azim Muradov <azim.muradov.dev@gmail.com>
2023-06-13 06:03:12 +0300

    Update version to 0.7.4

--- buildSrc/src/main/kotlin/app.kt
+++ buildSrc/src/main/kotlin/app.kt
@@ -4,10 +4,10 @@ object App {
 
     const val NAME: String = "stardew-valley-designer"
 
-    const val VERSION: String = "0.7.3"
+    const val VERSION: String = "0.7.4"
 
+    const val DESCRIPTION: String =
+        "The goal of this project is to provide a finely tuned editor for designing your farm and the interior of all its buildings."
 
-    const val DESCRIPTION: String = "The goal of this project is to provide a finely tuned editor for designing your farm and the interior of all its buildings."
-
-    const val COPYRIGHT: String = "Copyright © 2021-2023 Azim Muradov. Distributed under the Apache 2.0 license."
+    const val COPYRIGHT: String = "Copyright (c) 2021-2023 Azim Muradov."
 }
