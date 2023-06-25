41a4e78e1278f9120b18f110e13bb14b0b76f11f
Azim Muradov <azim.muradov.dev@gmail.com>
2023-06-25 21:36:38 +0300

    Fix compilation error

--- common/editor-engine/src/main/kotlin/io/stardewvalleydesigner/engine/geometry/shapes/PlacedDiamondStrategy.kt
+++ common/editor-engine/src/main/kotlin/io/stardewvalleydesigner/engine/geometry/shapes/PlacedDiamondStrategy.kt
@@ -19,8 +19,7 @@ package io.stardewvalleydesigner.engine.geometry.shapes
 import io.stardewvalleydesigner.engine.geometry.Coordinate
 
 
-            addAll(leftBorder)
-            addAll(rightBorder)
+object PlacedDiamondStrategy : PlacedShapeStrategy {
 
     override fun coordinates(a: Coordinate, b: Coordinate): Set<Coordinate> = FillingAlgorithm.fill(
         outline = PlacedDiamondOutlineStrategy.coordinates(a, b)
