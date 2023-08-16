b602f6b6353a954809f694dde40adcd84e55ba3b
Azim Muradov <azim.muradov.dev@gmail.com>
2023-08-17 00:01:25 +0300

    Add an icon for the "eye dropper"

--- app-desktop/src/main/kotlin/io/stardewvalleydesigner/screens/editor/sidemenus/Toolbar.kt
+++ app-desktop/src/main/kotlin/io/stardewvalleydesigner/screens/editor/sidemenus/Toolbar.kt
@@ -91,7 +91,7 @@ fun Toolbar(
                     ToolType.Pen -> "tools/pen.png"
                     ToolType.Eraser -> "tools/eraser.png"
                     ToolType.Select -> "tools/select.png"
-                    ToolType.EyeDropper -> "tools/hand.png" // TODO : "tools/eye-dropper.png"
+                    ToolType.EyeDropper -> "tools/eye-dropper.png"
                 }
             )
         }
