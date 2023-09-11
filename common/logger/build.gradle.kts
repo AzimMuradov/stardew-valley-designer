b7a88e970b20a42568301e774d46abc61c7bc55f
Azim Muradov <azim.muradov.dev@gmail.com>
2023-09-11 18:53:52 +0300

    Fix logger

--- common/logger/build.gradle.kts
+++ common/logger/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation(libs.kotlinlogging.jvm)
     runtimeOnly(libs.log4j.slf4j2)
+    compileOnly(libs.log4j.core)
 
     implementation(libs.mvikotlin)
     implementation(libs.mvikotlin.main)
