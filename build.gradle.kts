9c09b55d1e1a25cbbbe74db3493fb88cd432f90e
Azim Muradov <azim.muradov.dev@gmail.com>
2023-10-06 21:24:45 +0300

    Clean up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ plugins {
 subprojects {
     repositories {
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     tasks.withType<KotlinJvmCompile>().configureEach {
