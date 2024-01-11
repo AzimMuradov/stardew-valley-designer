5518886208b9b6a8259884e27125afd59e1dc5b8
Azim Muradov <azim.muradov.dev@gmail.com>
2024-01-11 23:42:03 +0300

    Remove unused deps

--- common/components/settings/build.gradle.kts
+++ common/components/settings/build.gradle.kts
@@ -13,9 +13,7 @@ kotlin {
             dependencies {
                 implementation(projects.common.editorEngine)
 
-                implementation(projects.common.components.screenMainMenu)
                 implementation(projects.common.components.screenEditor)
-                // implementation(projects.common.components.screenSettings)
 
 
                 // Meta-code
