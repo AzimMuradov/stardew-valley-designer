c1aae183d3c26c9752acf0eed11116aac7551187
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2024-04-23 15:09:11 +0600

    refactoring and little improvements

--- js/load-common.js
+++ js/load-common.js
@@ -1,13 +1,32 @@
-addElementToPage("header.html", "header-container")
-addElementToPage("footer.html", "footer-container")
+// Path to the folder with common HTML elements
+const COMMON_HTML_FOLDER = "common";
 
 
-function addElementToPage(htmlFile, elementID) {
-    document.addEventListener("DOMContentLoaded", function() {
-        fetch(htmlFile)
-            .then(response => response.text())
+function getPath(...parts) {
+    return parts.join("/");
+}
+
+
+function addElementToPage(htmlFileName, elementID) {
+        const path = getPath(COMMON_HTML_FOLDER, htmlFileName)
+
+        fetch(path)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.text();
+            })
             .then(data => {
                 document.getElementById(elementID).innerHTML = data;
+            })
+            .catch(error => {
+                console.error("Failed to load the HTML file:", error);
             });
-    });
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    addElementToPage("header.html", "header-container")
+    addElementToPage("footer.html", "footer-container")
+});
