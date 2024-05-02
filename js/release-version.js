9b19deadaac62fbf0839cb810172272257aab15e
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2024-05-03 01:45:14 +0600

    added caching

--- js/release-version.js
+++ js/release-version.js
@@ -1,41 +1,72 @@
 const OWNER = "AzimMuradov";
 const REPO = "stardew-valley-designer";
+const CACHE_TIME = 60; // 1 minute timeout for GitHub API requests
 
 
 function fetchLatestRelease(owner, repo) {
     const url = `https://api.github.com/repos/${owner}/${repo}/releases/latest`;
+    let cache = null;
+
+    try {
+        cache = JSON.parse(localStorage.getItem("apiCache"));
+    } catch(e) {
+        console.error("Error parsing JSON from localStorage:", e)
+    }
+
+    const now = new Date();
+
+    if (cache && cache.latestVersion && (now - new Date(cache.lastCheck)) / 1000 < CACHE_TIME) {
+        updateUI(cache.version, url);
+        return;
+    }
 
     fetch(url)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error("Network response was not ok");
             }
             return response.json();
         })
         .then(data => {
             const version = data.tag_name.slice(1);
 
-            const releaseVersionElement = document.getElementById("release-version");
-            if (releaseVersionElement) {
-                releaseVersionElement.innerText = version;
-            }
-
-            const updateHref = (id, newHref) => {
-                const element = document.getElementById(id);
-                if (element) {
-                    element.href = newHref;
-                }
+            const newCache = {
+                lastCheck: new Date().toISOString(),
+                version: version,
             };
 
-            updateHref("download-zip", `https://github.com/${owner}/${repo}/releases/latest/download/stardew-valley-designer-${version}-windows-amd64.zip`);
-            updateHref("download-msix", `https://github.com/${owner}/${repo}/releases/latest/download/stardew-valley-designer-${version}.x64.msix`);
-            updateHref("download-deb", `https://github.com/${owner}/${repo}/releases/latest/download/stardew-valley-designer_${version}_amd64.deb`);
-            updateHref("download-tar-gz", `https://github.com/${owner}/${repo}/releases/latest/download/stardew-valley-designer-${version}-linux-amd64.tar.gz`);
+            localStorage.setItem("apiCache", JSON.stringify(newCache));
+            updateUI(version, url);
         })
         .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
+            console.error("There was a problem with the fetch operation:", error);
         });
 }
 
+
+function updateUI(version) {
+    const updateText = (id, text) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.innerText = text;
+        }
+    };
+
+    const updateHref = (id, newHref) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.href = newHref;
+        }
+    };
+
+    const url = `https://github.com/AzimMuradov/stardew-valley-designer/releases/download/v${version}/stardew-valley-designer`
+
+    updateText("release-version", version);
+    updateText("release-version-linux", version);
+    updateHref("download-zip", `${url}-${version}-windows-amd64.zip`);
+    updateHref("download-msix", `${url}-${version}.x64.msix`);
+    updateHref("download-deb", `${url}_${version}_amd64.deb`);
+    updateHref("download-tar-gz", `${url}-${version}-linux-amd64.tar.gz`);
+}
 
 fetchLatestRelease(OWNER, REPO);
