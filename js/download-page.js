4ba8441cecfac3689317e4c21934818f0e25b283
Azim Muradov <azim.muradov.dev@gmail.com>
2024-05-03 01:28:17 +0300

    Fix paths on the site (#126)

--- js/download-page.js
+++ js/download-page.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const os = result.os.name;
 
         if (os === "Linux" && btnDownload) {
-            btnDownload.href = "https://stardewdesigner.com/pages/download-linux/";
+            btnDownload.href = "https://stardewdesigner.com/download-linux";
         }
     } catch (error) {
         console.error("Error parsing user agent:", error);
