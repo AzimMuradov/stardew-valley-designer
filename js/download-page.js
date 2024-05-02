d6c55416845e6c7092436859074e3766ae02d7d0
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2024-05-03 01:44:19 +0600

    added a definition of the client platform

---
+++ js/download-page.js
@@ -0,0 +1,15 @@
+const btnDownload = document.getElementById("download-btn");
+
+document.addEventListener('DOMContentLoaded', () => {
+    try {
+        const parser = new UAParser();
+        const result = parser.getResult();
+        const os = result.os.name;
+
+        if (os === "Linux" && btnDownload) {
+            btnDownload.href = "https://stardewdesigner.com/pages/download-linux/";
+        }
+    } catch (error) {
+        console.error("Error parsing user agent:", error);
+    }
+});
