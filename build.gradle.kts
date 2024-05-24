8b1fc485f1e60fd2984f59a814e50bd528682b38
Azim Muradov <azim.muradov.dev@gmail.com>
2024-05-24 17:01:51 +0300

    Move to Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.compose) apply false
 }
 
