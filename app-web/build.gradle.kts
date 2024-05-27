e142849bd7d2ff6417d22aff7ce08fb4c3d65c6d
Azim Muradov <azim.muradov.dev@gmail.com>
2024-05-27 19:48:03 +0300

    Move to Kotlin 2.0 (#135)

--- app-web/build.gradle.kts
+++ app-web/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.compose)
 }
 
@@ -77,31 +78,4 @@ kotlin {
 }
 
 
-// Resource management
-
-val copyWebResources by tasks.registering(Copy::class) {
-    from(
-        "../common/ui-components/editor/build/processedResources/wasmJs/main",
-        "../common/ui-components/themes/build/processedResources/wasmJs/main",
-    )
-    into("build/processedResources/wasmJs/main")
-
-    dependsOn(":app-web:wasmJsMainClasses")
-}
-
-tasks.named("compileDevelopmentExecutableKotlinWasmJs").configure {
-    dependsOn(copyWebResources)
-}
-
-tasks.named("compileProductionExecutableKotlinWasmJs").configure {
-    dependsOn(copyWebResources)
-}
-
-tasks.named("wasmJsJar").configure {
-    dependsOn(copyWebResources)
-}
-
-
-compose.experimental.web {
-    application {}
-}
+compose.web {}
